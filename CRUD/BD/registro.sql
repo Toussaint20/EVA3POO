27503a93ad39b60532f1b249b1ea7dd7a45fb746
Tomás Cid <39139189+Toussaint20@users.noreply.github.com>
2023-11-02 08:38:15 -0300

    Update registro.sql

--- CRUD/BD/registro.sql
+++ CRUD/BD/registro.sql
@@ -1,22 +1,27 @@
-DROP SCHEMA IF EXISTS `agenda` ;
-CREATE SCHEMA IF NOT EXISTS `agenda`   ;
-USE `agenda` ;
+DROP SCHEMA IF EXISTS `courier` ;
+CREATE SCHEMA IF NOT EXISTS `courier`   ;
+USE `courier` ;
  
 
 -- -----------------------------------------------------
--- Table `feria`.`comunas`
+-- sql para el courier
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Registro` ;
 
 CREATE TABLE IF NOT EXISTS `Registro` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(64) NOT NULL,
-  `Telefono` int(9) NOT NULL,
-  PRIMARY KEY (`Id`)
+  'Id' int(11) NOT NULL AUTO_INCREMENT,
+  'Destinatario' varchar(64) NOT NULL,
+  'Direccion' varchar(64) NOT NULL,
+  'C_origen' varchar(64) NOT NULL,
+  'C_destino' varchar(64) NOT NULL,
+  'Peso' int(9) NOT NULL,
+
+  PRIMARY KEY ('Id')
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-INSERT INTO `Registro`(`Nombre`, `Telefono`) VALUES
-('Juan', 99750000),
-('Maria',93405835),
-('Karina',90242342),
-('Marta',98234234)
+INSERT INTO 'Registro'('Nombre', 'Direccion', 'C_origen', 'C_destino', 'Peso',) VALUES
+('John Cena', 'Pablo Neruda 331', 'Santiago', 'Rancagua', '66'),
+('Tomás Cid', 'Blanco Encalada 210', 'Tokyo', 'Gorbea', '44.5'),
+('Tokoyami Towa', 'Hololive Corp 241', 'Gorbea', 'Tokyo', '44.5'),
+('Elton Jon', 'Los Poetas 345', 'Valdivia', 'Puerto Mont', '223'),
+('Keanu Reeves', 'Pasaje Vega 02147', 'Santiago', 'Temuco', '12')
