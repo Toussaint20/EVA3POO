fda55e8a01444c07465477e44b250bc0c9c6b1a4
Tomás Cid <39139189+Toussaint20@users.noreply.github.com>
2023-11-04 23:52:24 -0300

    Add files via upload

---
+++ CRUD/clase.py
@@ -0,0 +1,171 @@
+class Encomienda():
+    def __init__(self,id,destinatario, direccion, c_origen, c_destino, peso):
+        self.id = id
+        self.destinatario = destinatario
+        self.direccion = direccion
+        self.c_origen = c_origen
+        self.c_destino = c_destino
+        self.peso = peso
+
+    
+    #método para entregar datos a la BD
+    def returnArray(self): 
+        return [self.id,self.destinatario,self.direccion,self.c_origen,self.c_destino,self.peso]
+
+class Courier():
+    
+    encomiendas=[]
+
+    #metodos que obtienen datos
+
+    def listarEncomiendas(self):
+        print("\nEncomiendas: \n")
+        for con in self.encomiendas:
+            datos = "ID: {0}, Destinatario: {1}, Direccion: {2}"
+            print(datos.format(con.id, con.destinatario, con.direccion))
+        print(" ")
+    
+    def idExiste(self,id):
+        existeId = False
+        c=0
+        for con in self.encomiendas:
+            if con.id == id:
+                existeId = True
+                break
+            c += 1
+        if existeId:
+            indice = c
+        else:
+            indice = -1
+        return indice #retorna -1 si no está, sino, retorna indice en donde está esa id en el arreglo
+
+    def listarEncomienda(self):
+        self.listarEncomiendas()
+        NumeroCorrecto = False
+        while(not NumeroCorrecto):
+            idDetalle = input("Ingrese el ID del envío a revisar: ")
+            if idDetalle.isnumeric():
+                if (int(idDetalle) > 0):
+                    NumeroCorrecto = True
+                    idDetalle = int(idDetalle)
+                else:
+                    print("El Id debe ser mayor a 0.")
+            else:
+                print("debe ingresar un número.")
+   #     existeId=self.idExiste(idListar)
+
+    #    if existeId > -1:
+     #       print("ingrese datos a modificar")
+      #      encomienda=Courier.pedirDatosRegistro(idListar)
+      #      print(self.encomiendas[existeId])
+      #  else:
+            encomienda = None
+
+        return encomienda
+        
+
+    #métodos que modifican el Courier
+
+    def addEncomienda(self,encomienda):
+        self.encomiendas.append(encomienda)
+    
+    def delEncomienda(self,id):
+        c=0
+        for re in self.encomiendas:
+            if re.id == id:
+                del self.encomiendas[c]
+                break
+            c += 1
+
+    #método para obtener los datos de un contacto por teclado
+
+    @staticmethod
+    def pedirDatosRegistro(id):
+    
+        destinatario = input("Ingrese nombre de destinatario: ")
+        direccion = input("Ingrese dirección de envío:")
+        c_origen = input("Ingrese ciudad de origen de envío:")
+        c_destino = input("Ingrese ciudad de destino de envío:")
+        NumeroCorrecto = False
+        while(not NumeroCorrecto):
+            peso = input("Ingrese peso de envío: ")
+            if peso.isnumeric():
+                if (int(peso) > 0 and len(peso)<10):
+                    NumeroCorrecto = True
+                    peso = int(peso)
+                else:
+                    print("El número debe ser mayor a 0 y de 9 dígitos máximo.")
+            else:
+                print("Número incorrecto: Debe ser un número Únicamente.")
+        encomienda = Encomienda(id,destinatario, direccion, c_origen, c_destino, peso)
+        return encomienda
+    
+    @staticmethod
+    def pedirDatosActDir(id):
+    
+        direccion = input("Ingrese dirección de envío:")
+        
+        encomienda = Encomienda(id, direccion)
+        return encomienda
+
+    #métodos para pedir al usuario los datos para usar el CRUD
+
+    def agregarEncomienda(self):
+        id=0
+        for con in self.encomiendas:
+            if con.id > id:
+                id = con.id
+        encomienda=Courier.pedirDatosRegistro(id+1) #esto asegura que el id es mayor al último registrado
+        self.addEncomienda(encomienda) #agrega al contacto a la lista en el obj
+        return encomienda
+
+    def actualizarEncomienda(self):
+        self.listarEncomiendas()
+        
+        NumeroCorrecto = False
+        while(not NumeroCorrecto):
+            idEditar = input("Ingrese el ID del contacto a editar: ")
+            if idEditar.isnumeric():
+                if (int(idEditar) > 0):
+                    NumeroCorrecto = True
+                    idEditar = int(idEditar)
+                else:
+                    print("El Id debe ser mayor a 0.")
+            else:
+                print("debe ingresar un número.")
+        
+        existeId=self.idExiste(idEditar)
+
+        if existeId > -1:
+            print("ingrese datos a modificar")
+            encomienda=Courier.pedirDatosActDir(idEditar)
+            self.encomiendas[existeId]=encomienda #modifica el contacto en el objeto
+        else:
+            encomienda = None
+
+        return encomienda
+
+
+    def eliminarEncomienda(self):
+        self.listarEncomiendas()
+        
+        NumeroCorrecto = False
+        while(not NumeroCorrecto):
+            idEliminar = input("Ingrese el id del contacto a eliminar: ")
+            if idEliminar.isnumeric():
+                if (int(idEliminar) > 0):
+                    NumeroCorrecto = True
+                    idEliminar = int(idEliminar)
+                else:
+                    print("El Id debe ser mayor a 0.")
+            else:
+                print("debe ingresar un número.")
+
+        existeId=self.idExiste(idEliminar)
+
+        if existeId == -1:
+            idEliminar = 0
+        else:
+            del self.encomiendas[existeId] #elimina el contacto de la lista en el obj
+
+        return idEliminar
---
+++ CRUD/main.py
@@ -0,0 +1,116 @@
+from BD.conn import DAO 
+#requiere un archivo en carpeta BD con nombre __init__.py 
+#(este archivo indica qué importaría de esta carpeta, si está vacío importa todo)
+from clase import Courier, Encomienda
+
+def actualizarCourier():
+    arrayEncomiendas = dao.listarEncomiendas() #saca los encomiendas de la BD y los deja en un array de tuplas 
+    courier.encomiendas=[] #vacía la courier
+    for con in arrayEncomiendas: #los pone en el objeto courier como objetos encomienda
+       courier.addEncomienda(Encomienda(con[0],con[1],con[2],con[3],con[4],con[5]))
+
+def login():
+    continuar = True #continuar = true para que al cerrar sesión vuelva al login
+    while continuar:
+        print("=== Inicio de Sesión ===")
+        usuario = input("Ingrese su ID de usuario: ")
+        password = input("Ingrese su contraseña: ")
+
+        resultados = dao.Login(usuario, password)
+        print(resultados)
+        if resultados:
+            menuPrincipal()  
+        else:
+            print("ID de usuario o contraseña incorrectos. Intente nuevamente.")  
+
+def menuPrincipal():
+    continuar = True
+    while(continuar):
+        opcionCorrecta = False
+        while(not opcionCorrecta):
+            print("==================== MENÚ PRINCIPAL ====================")
+            print("1.- Listar encomiendas")
+            print("2.- Registrar encomienda")
+            print("3.- Actualizar dirección de envío de encomienda")
+            print("4.- Eliminar encomienda")
+            print("5.- Revisar detalle de encomienda")
+            print("6.- Cerrar sesión")
+            print("========================================================")
+            opcion = int(input("Seleccione una opción: "))
+
+            if opcion < 1 or opcion > 6:
+                print("Opción incorrecta, ingrese nuevamente...")
+            elif opcion == 6:
+                continuar = False
+                print("¡Gracias por usar la aplicación de Courier!!!")
+                actualizarCourier()
+                break
+            else:
+                opcionCorrecta = True
+                ejecutarOpcion(opcion)
+
+
+def ejecutarOpcion(opcion):
+    
+    if opcion == 1:
+        try:
+            #listar
+            if len(courier.encomiendas) > 0:
+                courier.listarEncomiendas()
+            else:
+                print("No se encontraron Encomiendas...")
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 2:
+        #registrar
+        encomienda = courier.agregarEncomienda()
+        try:
+            dao.registrarEncomiendas(encomienda.returnArray())
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 3:
+        try:
+            #actualizar
+            if len(courier.encomiendas) > 0:
+                encomienda = courier.actualizarEncomienda()
+                if encomienda:
+                    dao.actualizarEncomiendas(encomienda.returnArray())
+                else:
+                    print("Id de encomienda a actualizar no encontrado...\n")
+            else:
+                print("No se encontraron encomiendas...")
+        except:
+            print("Ocurrió un error...")
+    elif opcion == 4:
+        try:
+            #eliminar
+            if len(courier.encomiendas) > 0:
+                idEliminar = courier.eliminarEncomienda()
+                if not(idEliminar == 0):
+                    dao.eliminarEncomiendas(idEliminar)
+                else:
+                    print("Id de encomienda no encontrada...\n")
+            else:
+                print("No se encontraron encomiendas...")
+        except:
+            print("Ocurrió un error...")    
+    elif opcion == 5:
+        try:
+            #listar al detalle
+            if len(courier.encomiendas) > 0:
+                idDetalle = courier.listarEncomienda()
+                if not(idDetalle == 0):
+                    dao.listarEncomienda(idDetalle)
+                else:
+                    print("Id de encomienda no encontrada...\n")
+            else:
+                print("No se encontraron encomiendas...")
+        except:
+            print("Ocurrió un error...")    
+        
+    
+
+courier = Courier()
+dao = DAO()
+actualizarCourier() #ponemos los datos de la BD en el objeto agenda
+login()
